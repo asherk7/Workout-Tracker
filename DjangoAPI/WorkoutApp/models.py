5d1787b806b6d61e74cc1c1cb4845d9bc85615d9
asherk7 <masherk03@hotmail.com>
2023-05-12 03:57:57 -0400

    Created Django project and app

---
+++ DjangoAPI/WorkoutApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Workouts(models.Model):
+    pass
+
+class PersonalRecords(models.Model):
+    pass
+
+class Muscles(models.Model):
+    pass
+
+class Splits(models.Model):
+    pass
