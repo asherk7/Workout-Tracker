36adab1afc80696eaec836bccbfb4ad11c70efaa
asherk7 <masherk03@hotmail.com>
2023-05-14 19:29:10 -0400

    added routing

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { WorkoutsComponent } from './workouts/workouts.component';
+import { PersonalrecordsComponent } from './personalrecords/personalrecords.component';
+import { MusclesComponent } from './muscles/muscles.component';
+import { SplitsComponent } from './splits/splits.component';
+
+const routes: Routes = [
+  { path: 'workouts', component: WorkoutsComponent },
+  { path: 'personalrecords', component: PersonalrecordsComponent },
+  { path: 'muscles', component: MusclesComponent },
+  { path: 'splits', component: SplitsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
